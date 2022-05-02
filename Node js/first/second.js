08ce5009621ce5082181349f0477a0ac0269a8a9
Dibyakanta1998 <104753435+Dibyakanta1998@users.noreply.github.com>
2022-05-02 13:02:33 +0530

    Delete Node js directory

--- Node js/first/second.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-var data = fs.readFileSync("mytxt.txt");
-//fs.readFileSync("mytxt.txt");
-console.log(data.toString());
-console.log("program ended");
--- Node js/mongotonode/schema/schema1.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-var userSchema = mongoose.Schema(
-  {
-    name: { type: String },
-    password: { type: String },
-  },
-  { versionKey: false }
-);
-module.exports = mongoose.model("details", userSchema);
